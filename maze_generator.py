186a47e0923d8992a30927d8d039e0373be237a2
John Jermanis <33067902+jjermanis@users.noreply.github.com>
2017-11-13 13:37:50 -0800

    Add files via upload

--- maze_generator.py
+++ maze_generator.py
@@ -18,10 +18,15 @@ WALL_COLOR = (0, 0, 0)
 FLOOR_COLOR = (255, 255, 255)
 
 
+# TODO: put this into a class to clean up some logic (shared state, recalculated value)
+
+
 def generate_maze(width, height):
     # Initialize grid.  Start state is with each cell having no openings.
     grid = [[0 for _ in range(width+2)] for _ in range(height+2)]
 
+    # Create the outer wall of the maze.  Each outer cell has openings to the other cells, except
+    # into the main body of the maze.
     for x in range(0, width+2):
         grid[0][x] = LEFT+RIGHT+UP
         grid[height+1][x] = LEFT+RIGHT+DOWN
@@ -29,11 +34,17 @@ def generate_maze(width, height):
         grid[y][0] |= LEFT+UP+DOWN
         grid[y][width+1] |= RIGHT+UP+DOWN
 
-    # Generate maze from random starting point on the top.
+    # Generate maze from random starting point on the top.  Use "hunt and kill" maze gen algorithm.
     maze_start = random.randint(1, width-1)
-    grid = fill_maze_from(maze_start, 1, grid)
+    x = maze_start
+    y = 1
+    done = False
+    while not done:
+        grid = fill_maze_from(x, y, grid)   # Fill maze until a dead-end is hit
+        x, y = hunt(grid)             # Look for empty spots to continue to gen maze
+        done = x is None
 
-    # Create an opening on top and bottom.
+    # Create openings on top and bottom.
     # TODO: be smart about where to put bottom exit to maximize maze length
     grid[1][maze_start] += UP
     grid[0][maze_start] += DOWN
@@ -44,21 +55,57 @@ def generate_maze(width, height):
     return grid
 
 
+def get_neighbors_random(x, y, grid):
+    """
+    :return: A randomly ordered list of the neighbor cells of (x, y).  Each element is a tuple with 4 values:
+    neighbor x - the x-value of the neighbor cell
+    neighbor y - the y-value of the neighbor cell
+    direction - the direction from (x, y) to the neighbor cell
+    opposite - the direction from the neighbor cell to (x, y)
+    """
+    neighbors = [(x - 1, y, LEFT, RIGHT), (x, y + 1, DOWN, UP), (x + 1, y, RIGHT, LEFT), (x, y - 1, UP, DOWN)]
+    random.shuffle(neighbors)
+    return neighbors
+
+
 def fill_maze_from(x, y, grid):
     """
-    Uses recursive backtracking algorithm to fill grid with maze.  There will be no cycles, and every cell will be
-    connected in the same network.
+    From the start point, create a path using cells that have not been previously visitied.  Returns when a
+    dead-end is reached.
     :return: Grid with values for open passage ways
     """
-    #TODO: convert to iterative to avoid stack overflow
-    directions = [(x - 1, y, LEFT, RIGHT), (x, y + 1, DOWN, UP), (x + 1, y, RIGHT, LEFT), (x, y - 1, UP, DOWN)]
-    random.shuffle(directions)
-    for new_x, new_y, direction, opposite in directions:
-        if 1 <= new_x <= len(grid[0]) - 2 and 1 <= new_y <= len(grid) - 2 and grid[new_y][new_x] == 0:
-            grid[y][x] += direction
-            grid[new_y][new_x] = opposite
-            grid = fill_maze_from(new_x, new_y, grid)
+    foundNeighbor = True  # this is logically a "do...while", but since this is Python, we'll fake it.
+    while foundNeighbor:
+        foundNeighbor = False
+        neighbors = get_neighbors_random(x, y, grid)
+        # Look for a neighbor (in bounds) that has not already been visited
+        for new_x, new_y, direction, opposite in neighbors:
+            if 1 <= new_x <= len(grid[0]) - 2 and 1 <= new_y <= len(grid) - 2 and grid[new_y][new_x] == 0:
+                # Next cell found.  Connect to new cell, and continue filling from that new cell.
+                foundNeighbor = True
+                grid[y][x] += direction
+                grid[new_y][new_x] = opposite
+                x = new_x
+                y = new_y
+                break
     return grid
+
+
+def hunt(grid):
+    """
+    Look for a place to start a new path.  It needs to be an unvisited cell that has at least
+    one visited neighbor.  Returns a tuple with a new cell to draw a path from.
+    """
+    for y in range(len(grid)):
+        for x in range(len(grid[0])):
+            if (grid[y][x] == 0):
+                neighbors = get_neighbors_random(x, y, grid)
+                for new_x, new_y, direction, opposite in neighbors:
+                    if 1 <= new_x <= len(grid[0]) - 2 and 1 <= new_y <= len(grid) - 2 and grid[new_y][new_x] != 0:
+                        grid[y][x] += direction
+                        grid[new_y][new_x] += opposite
+                        return x, y
+    return None, None
 
 
 def convert_to_bitmap(maze):
@@ -89,6 +136,7 @@ def convert_to_bitmap(maze):
 
 def save_bitmap(bitmap, filename):
     # Convert 2D bitmap to 1D array of pixels
+    # TODO: does PIL take a 2D array?
     pixel_list = []
     for pixel_y in range(len(bitmap)):
         for pixel_x in range(len(bitmap[0])):
@@ -103,8 +151,8 @@ def save_bitmap(bitmap, filename):
 def main():
     random.seed()
     print("Maze generator")
-    width = input_int("Width? ", 3, 50)
-    height = input_int("Height? ", 3, 50)
+    width = input_int("Width? ", 3, 500)
+    height = input_int("Height? ", 3, 500)
     maze = generate_maze(width, height)
     print("Maze generated.")
     filename = input("File name? ")
